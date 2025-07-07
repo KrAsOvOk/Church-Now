5ef1ef6a37357a28be064fdd391f74b30ee847c4
oleksandr_martyn <xbox37206072@gmail.com>
2025-07-08 01:15:03 +0200

    +

--- src/app/api/members/route.ts
+++
@@ -1,8 +0,0 @@
-import clientPromise from "@/lib/mongodb";
-
-export async function GET() {
-  const client = await clientPromise;
-  const db = client.db("AllData"); // Use your database name
-  const members = await db.collection("Events").find({}).toArray();
-  return new Response(JSON.stringify(members), { status: 200 });
-}
